68e4c0f25481ff07088e4ef4d561f94a412009e7
brian d foy <briandfoy@pobox.com>
2026-05-06 23:41:22 -0400

    initial import

---
+++ lib/Module/Release/GitHub/Workflows.pm
@@ -0,0 +1,65 @@
+package Module::Release::GitHub::Workflows;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+Module::Release::GitHub::Workflows - block releases if GitHub workflows have failed
+
+=head1 SYNOPSIS
+
+	use Module::Release::GitHub::Workflows;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/
+
+=head1 AUTHOR
+
+brian d foy, C<< <briandfoy@pobox.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2026-2026, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
